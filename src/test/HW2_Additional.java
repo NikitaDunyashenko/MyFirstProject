9136f37504166a13acf2f7f80f831b1961ac8a94
nikita <dunyashenkonikita@mail.ru>
2022-10-22 16:58:49 +0300

    changes in condition

--- src/test/HW2_Additional.java
+++ src/test/HW2_Additional.java
@@ -98,7 +98,7 @@ public class HW2_Additional {
                 }
             }
         }
-        if (index == true) {
+        if (index == false) {
             System.out.println("Elements of the array are repeated");
         } else {
             System.out.println("Elements of the array are not repeated");
